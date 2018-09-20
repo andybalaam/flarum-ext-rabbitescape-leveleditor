b9c08413fcbedd64b5ae47c2c3927ff129db1aa5
Andy Balaam <andybalaam@artificialworlds.net>
2018-09-20 23:48:05 +0100

    Break level editor view div prep into functions

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -2,56 +2,67 @@ import { extend } from 'flarum/extend';
 import Post from 'flarum/components/Post';
 
 
-var editorId = 0;
+var viewId = "levelEditorView";
 
-function prepLevelEditor(eid){
-
-    var loadLevelEditor = function(){
-        var c = document.querySelector("code.language-rel");
+function loadLevelEditor(viewDiv) {
+    return function() {
+        // Find the first ```rel code block
+        var c = viewDiv.parentNode.querySelector("code.language-rel");
         if (!c) {;
-            return;
+            return;  // Nothing to do if there isn't one
         }
-        var div = document.getElementById(eid);
-        if (!div) {
-            div = document.createElement("div");
-            div.id = eid;
-            c.parentNode.appendChild(div);
+
+        if (!Elm){
+            return; //  Bug?
         }
+
+        // Run the Elm program, which replaces that div
         var app = Elm.Main.init({
-            node: div,
+            node: viewDiv,
             flags: {
                 worldText: c.innerHTML,
                 mode: "View",
                 urlPrefix: "/rabbit-escape/level-editor/",
-                id: eid,
+                id: viewId
             }
         });
     };
+};
 
-    return function() {
-        if (!document.getElementById("level-editor-style")) {
-            var style = document.createElement("link");
-            style.rel = "stylesheet";
-            style.href = "/rabbit-escape/level-editor/style.css";
-            style.id = "level-editor-style";
-            document.head.appendChild(style);
-        }
-        if (!document.getElementById("level-editor-script")) {
-            var script = document.createElement("script");
-            script.onload = loadLevelEditor;
-            script.src = "/rabbit-escape/level-editor/level-editor.js";
-            script.id = "level-editor-script";
-            document.head.appendChild(script);
-        } else {
-            loadLevelEditor();
-        }
+function findOrCreateStyleLink() {
+    if (!document.getElementById("level-editor-style")) {
+        var style = document.createElement("link");
+        style.rel = "stylesheet";
+        style.href = "/rabbit-escape/level-editor/style.css";
+        style.id = "level-editor-style";
+        document.head.appendChild(style);
     }
+}
+
+function findOrCreateScriptLink(callback) {
+    if (document.getElementById("level-editor-script")) {
+        callback();
+    } else {
+        var script = document.createElement("script");
+        script.onload = callback;
+        script.src = "/rabbit-escape/level-editor/level-editor.js";
+        script.id = "level-editor-script";
+        document.head.appendChild(script);
+    }
+}
+
+function prepLevelEditor(viewDiv) {
+    findOrCreateStyleLink();
+    findOrCreateScriptLink(loadLevelEditor(viewDiv));
 }
 
 app.initializers.add('rabbitescape-leveleditor', function() {
     extend(Post.prototype, 'view', function(vdom) {
-        editorId++;
-        var eid = 'leveleditor' + editorId;
-        vdom.children.push(m('div', {id: eid, config: prepLevelEditor(eid)}));
+        vdom.children.push(
+            m(
+                'div',
+                {id: viewId, config: prepLevelEditor}
+            )
+        );
     });
 });
